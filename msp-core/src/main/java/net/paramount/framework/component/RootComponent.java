0f464c67c5efb072a888f66ab5e4f9fda0514859
ducbui <ducbui@Duc-PC>
2019-11-01 06:38:20 +0700

    sa

---
+++ msp-core/src/main/java/net/paramount/framework/component/RootComponent.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package net.paramount.framework.component;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import net.paramount.framework.logging.LogService;
+
+/**
+ * @author bqduc
+ *
+ */
+public abstract class RootComponent implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4315018091652981743L;
+
+	@Inject
+	protected LogService log;
+}
---
+++ msp-core/src/main/java/net/paramount/framework/entity/BizEntity.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package net.paramount.framework.entity;
+
+/**
+ * @author bqduc
+ *
+ */
+public interface BizEntity extends EntityBase {
+	Boolean isVisible();
+	void setVisible(Boolean visible);
+}
